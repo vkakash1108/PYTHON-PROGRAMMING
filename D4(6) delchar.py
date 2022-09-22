e62849ca16d9c26ffa3cdb3367d408a27d680090
V Akash <113331439+vkakash1108@users.noreply.github.com>
2022-09-22 14:06:17 +0530

    Add files via upload

---
+++ D4(6) delchar.py
@@ -0,0 +1,10 @@
+def delchar(s,c):
+    
+    if (len(c)==1):
+        for i in range(len(s)):
+            if (c is not s[i]):
+                print( s[i])
+            else:
+                continue
+
+delchar('apple','b')
