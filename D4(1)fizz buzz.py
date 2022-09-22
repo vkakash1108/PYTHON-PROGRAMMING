3428111d6cf349947f7b83930babdf403e710c54
V Akash <113331439+vkakash1108@users.noreply.github.com>
2022-09-22 14:05:52 +0530

    Add files via upload

---
+++ D4(1)fizz buzz.py
@@ -0,0 +1,12 @@
+for fizzbuzz in range(1,10):
+
+ if fizzbuzz % 15 == 0:
+  print("FizzBuzz")          
+  continue
+ elif fizzbuzz % 3 == 0: 
+  print("Fizz")          
+  continue
+ elif fizzbuzz % 5 == 0:  
+  print("Buzz")         
+  continue
+ print()
